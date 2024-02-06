736a4d46003a00fa720d36a85656273a5fffb558
Keith Tang <pokpok9301@gmail.com>
2024-02-06 15:05:12 +0000

    add text practice

---
+++ text.py
@@ -0,0 +1,17 @@
+def text(): #3.1.2 text 
+    print(3 * 'un' + 'ium')#
+    print('Py' 'thon')
+    print('Put several strings within parentheses '
+        'to have them joined together.')
+
+def slice():
+    word = 'Python'
+    print(word[0])
+    print(word[4:42])
+    print(word[42:])
+
+if __name__ == "__main__":
+    text()
+    slice()
+
+    
