ef43d2b0af7369804267d5c20af0821ea18ec959
Keith Tang <pokpok9301@gmail.com>
2024-02-12 10:13:03 +0000

    sqlalchemy practice

---
+++ SQL_acad/connect.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine, text
+
+engine = create_engine("sqlite:///sample.db")
+
+with engine.connect() as connection:
+    result = connection.execute(text('select "Hello"'))
+
+    print(result.all())
